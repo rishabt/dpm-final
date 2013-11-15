a13ce25d4eb4262e67bebfe98254e4da09ac8abc
Rishabh Tandon <rishabh.tandon@mail.mcgill.ca>
2013-11-15 07:54:30 -0500

    Obstacle avoidance controlling everything and localization done

--- src/Master/Master.java
+++ src/Master/Master.java
@@ -7,23 +7,49 @@ import lejos.nxt.Button;
 public class Master{
 	
 	public static void main(String[] args){
-		Button.ESCAPE.addButtonListener(new Support.ExitListener());
 		
-		Button.waitForAnyPress();
-		
-		TwoWheeledRobot robot = new TwoWheeledRobot(Motor.A, Motor.B);
-		Odometer odo = new Odometer(robot, true);
-		Navigation nav = new Navigation(odo);
-		LCDInfo lcd = new LCDInfo(odo);
-		
-		Grid grid = new Grid(4 * 30, 4 * 30);
-		
-		
-		Point p = grid.getFreshPoint();
-		LCD.clear();
-		LCD.drawString(p.toString(), 0, 5);
-		
-		Button.waitForAnyPress();
+//			 setup the odometer, display, and ultrasonic and light sensors
+			TwoWheeledRobot patBot = new TwoWheeledRobot(Motor.A, Motor.B);
+			Odometer odo = new Odometer(patBot, true);
+			Navigation nav = new Navigation(odo);
+			LCDInfo lcd = new LCDInfo(odo);
+			UltrasonicSensor us = new UltrasonicSensor(SensorPort.S1);
+			LightSensor ls = new LightSensor(SensorPort.S3);
+			
+			if(Button.waitForAnyPress() == Button.ID_LEFT){
+				
+//				USLocalizer usl = new USLocalizer(odo, us, USLocalizer.LocalizationType.FALLING_EDGE);
+//				usl.doLocalization();
+				//try { Thread.sleep(2000); } catch (InterruptedException e) {}
+				
+				nav.travelTo(70, 60);
+				
+				nav.travelTo(20, 80);
+				
+				nav.travelTo(60, 80);
+				
+				nav.travelTo(60, 100);
+				
+				nav.travelTo(90, 10);
+				
+				nav.travelTo(120, 90);
+				
+				nav.travelTo(120, 120);
+				
+				nav.travelTo(150, 150);
+				
+				nav.travelTo(170, 170);
+				
+				nav.travelTo(100, 60);
+				
+				
+			}
+			
+			
+			
+			if(Button.waitForAnyPress() == Button.ID_ESCAPE){
+				System.exit(0);
+			}
 		
 	}
 	
--- src/Master/USLocalizer.java
+++ src/Master/USLocalizer.java
@@ -1,26 +1,21 @@
 package Master;
 
-import lejos.nxt.Button;
-import lejos.nxt.LCD;
 import lejos.nxt.Motor;
+import lejos.nxt.NXTRegulatedMotor;
 import lejos.nxt.Sound;
 import lejos.nxt.UltrasonicSensor;
 
 public class USLocalizer {
+	
 	public enum LocalizationType { FALLING_EDGE, RISING_EDGE };
-	public static double ROTATION_SPEED = 30;
-
+	public static double ROTATION_SPEED = 40;    
+	
 	private Odometer odo;
-	private Navigation nav;
 	private TwoWheeledRobot robot;
 	private UltrasonicSensor us;
 	private LocalizationType locType;
-	public boolean isRotating;
-	public boolean wallPresence;
-	public boolean wallAbsence;
-	public double[] position = new double[3];
-	public int distanceCheck;
-	private double newTheta; //change to private later
+	private final int walld = 50;
+	
 	
 	public USLocalizer(Odometer odo, UltrasonicSensor us, LocalizationType locType) {
 		this.odo = odo;
@@ -28,233 +23,167 @@ public class USLocalizer {
 		this.us = us;
 		this.locType = locType;
 		
-		this.nav = new Navigation(odo);
-		
 		// switch off the ultrasonic sensor
 		us.off();
 	}
 	
 	public void doLocalization() {
-		//double [] position = new double [3];
-		double angleA, angleB;
-		int filterControl = 0;
 		
-		try{
-			
+		double [] pos = new double [3];
+		double angleA = 0, angleB = 0;
+         double progress = 0;
+		   int distance = getFilteredData();
+		   boolean initial = false;
+		   boolean isanglefinished = false;
+		   
 		if (locType == LocalizationType.FALLING_EDGE) {
+			
 			// rotate the robot until it sees no wall
+		    // starting direction identification: if the starting feedback distance is more than 50,
+		   //  proceed to progress 1(initial = false && progress = 1); 
+		   // else, initiate the initial turning to make it more than 40(initial = true && progress =0)
+		    while( progress == 0 && initial == false){
+		    	distance = getFilteredData();
+		    	if(distance < walld){
+		    		initial = true;
+		    		progress = 0;		    		
+		    	}
+		    	else if (distance > walld ){
+		    		initial = false;
+		    		progress = 1;
+		    		Sound.beep();		    		
+		    		
+		    	}
+		    }
+		    
+		    //try { Thread.sleep(100); } catch (InterruptedException e) {}
+		    
+		    // initial process(progress = 0), if the distance is more than 50, go to progress 1
+		    while( progress == 0 && initial == true){
+		    	
+		    	distance = getFilteredData();
+		    	robot.setRotationSpeed(ROTATION_SPEED);
+		    	
+		    	if(distance > walld ){	    		
+		    		distance = getFilteredData();
+		    		progress = 1;
+		    		initial = false;//quit the initial process
+		    	Sound.beep();
+		    			    	
+		    	}	
+		    	
+		    }
+		    try { Thread.sleep(100); } catch (InterruptedException e) {}
+		    // ****progress 1 starts here: 
+		    // rotate until reaching a distance less than 40. when it does,
+		    // stop the vehicle temporarily and latch angleA.
+		    // then proceed to progress 2.
+		    while (progress == 1 && distance > walld-1 ){
+		    	
+                odo.getPosition(pos);
+		    	distance = getFilteredData();
+		    	robot.setRotationSpeed(ROTATION_SPEED);
+		    			    	
+		    	if(distance < walld+1){		    		
+		    		robot.setRotationSpeed(-ROTATION_SPEED);
+		    		angleA = pos[2];		    		
+		    		progress = 2;		    				    				    		
+		    	}
+		    			    	
+		    }
+		    
+			try { Thread.sleep(1000); } catch (InterruptedException e) {}
 			
-			if(getFilteredData() <= 32){									//Checks if the robot is facing the wall, in which case it has 
-				while(getFilteredData() <= 32){								//to filter out values to start with the falling edge algorithm
-					robot.setRotationSpeed(ROTATION_SPEED);
-					
-					if(getFilteredData() == 50){
-						filterControl ++;
-						Thread.sleep(500);
-					}
-				}
-				
-				
-				
-			}
+		    //rotate in the opposite direction(progress 2) after angleA is recorded.
+		    //then, when seeing the other wall, latch the angle.
+		    while (progress == 2){
+		    	
+		    	odo.getPosition(pos);
+		    	distance = getFilteredData();
+		    	robot.setRotationSpeed(-ROTATION_SPEED);
+		    	
+		    	
+		    	if(distance < walld ){		    		
+		    		angleB = 360 -pos[2];		    		
+		    		robot.setRotationSpeed(0.0);	
+		    		
+		    		try { Thread.sleep(80); } catch (InterruptedException e) {}
+		    		
+		    		progress = 3;//go to progress 3		    				    		
+		    	}
+		    }
+		    
+		    //progress == 3 is the calculation and turning to y=axis process.	
+		    if (progress == 3){
+		    	
+		    	double value = 0.0;
+		        //double test = (angleA-angleB)/2 - 45;
+		        
+		    	if(angleA > angleB){
+		    	   value = 45 - (angleA + angleB)/2;
+		    	   isanglefinished = true;
+		       }
+		       else if(angleB > angleA){
+		    	   value = 225 - (angleB + angleA)/2;
+		    	   
+		    	   try { Thread.sleep(80); } catch (InterruptedException e) {}
+		    	   
+		    	   isanglefinished = true;		    	   
+		       }
+		       
+		    	try { Thread.sleep(80); } catch (InterruptedException e) {}
+		    	
+		       if(isanglefinished == true){
+		       Motor.A.setSpeed(80);
+		       Motor.B.setSpeed(80);
+		       
+		         if(angleA < angleB){
+		    	   Motor.A.rotate(convertAngle(2.885, 16.755,  value-105), true);
+				   Motor.B.rotate(-convertAngle(2.885, 16.755, value-105),false);  
+		         }
+		         if(angleA > angleB){
+		           Motor.A.rotate(convertAngle(2.885, 16.755,  -value-6), true);
+				   Motor.B.rotate(-convertAngle(2.885, 16.755, -value-6),false);  
+		         }
+		         
+				   try { Thread.sleep(100); } catch (InterruptedException e) {}
+				   odo.getPosition(pos);
+				   boolean update[] = {true, true, true};
+				   double newpos[] = {0,0,0+1};
+				   odo.setPosition(newpos, update);
+		       //}
+		      // if(angleA > 180){
+		       //Motor.A.rotate(convertAngle(2.885, 16.755,  test ), true);
+			   //Motor.B.rotate(-convertAngle(2.885, 16.755, test),false);
+		       //}
+		       }
+		    }
+		
+		    
+			//keep rotating until the robot sees a wall, then latch the angle
 			
-			robot.setRotationSpeed(ROTATION_SPEED);
+			//angle latched here( seeing the first-taking-into-account wall)
 			
+			// switch direction and rotate 
 			
-			
-			if(filterControl >= 5){
-				robot.setRotationSpeed(0);
-			}
-			
-			
-			robot.setRotationSpeed(ROTATION_SPEED);						//Robot starts rotating
-			isRotating = true;											//Important variables initialized to their desired values
-			wallAbsence = true;
-			wallPresence = false;
-			
-			robot.setRotationSpeed(ROTATION_SPEED);
-			
-			while(wallAbsence){											//Condition when wall is absent 
-				
-				if(getFilteredData() < 40){								//If the sensor senses a wall nearby, it reverses the conditions
-					wallAbsence = false;
-					wallPresence = true;
-				}
-			}
-			
-			//robot.setRotationSpeed(0);
-			
-			odo.getPosition(position);									//The exact angle of the position of the wall is stored in angleA
-			angleA = position[2];
-			distanceCheck = getFilteredData();
-			//LCD.drawString("" + angleA, 0, 4);
-			
-			robot.setRotationSpeed(-ROTATION_SPEED);					//The rotation is reversed to check the wall on the other side
-			wallPresence = false;										//Conditions reset
-			wallAbsence = true;
-			
-			Thread.sleep(500);											
-			
-			
-			while(wallAbsence){											//While wall is absent the loop is run
-				while(wallAbsence){
-					
-					if(getFilteredData() < 40){							//If a wall is detected, the conditions are reversed
-						wallAbsence = false;
-						wallPresence = true;
-					}
-				}
-			}
-			
-			robot.setRotationSpeed(0);									//The motors stop rotating at that point
-			
-			odo.getPosition(position);									//The position of the second wall is stored in angleB
-			angleB = 360 - position[2];
-			//LCD.drawString("" + angleB, 0, 5);
-			
-			if(angleA > angleB){										//Conditions to calculate the newTheta
-				newTheta = 44 - (angleA + angleB)/2;
-				//LCD.drawString("aa" + newTheta, 0, 6);
-			}
-			
-			else if(angleA < angleB){
-					newTheta = 200 + (angleA + angleB)/2;
-					//LCD.drawString("bb" + newTheta, 0, 6);
-			}
-			
-			else {
-				newTheta= 45;
-				//LCD.drawString("abc", 0, 6);
-			}
-			odo.getPosition(position);
-			double realPosition = ((newTheta)); 						//The odometer is set to the actual position it is at
-			//LCD.drawString("dd" + realPosition, 0, 7);
-			//Button.waitForAnyPress();
-			odo.setPosition(new double [] {0.0, 0.0, realPosition}, new boolean [] {false, false, true});	
-			//Button.waitForAnyPress();
-			nav.turnTo(0);
 			
 			// keep rotating until the robot sees a wall, then latch the angle
 		
-		
-			
-			
-			// switch direction and wait until it sees no wall
-			
-			// keep rotating until the robot sees a wall, then latch the angle
-			
 			// angleA is clockwise from angleB, so assume the average of the
 			// angles to the right of angleB is 45 degrees past 'north'
-			
-			// update the odometer position (example to follow:)
-			//odo.setPosition(new double [] {0.0, 0.0, 0.0}, new boolean [] {true, true, true});
-		} 
-		
-		else {
-			/*
-			 * The robot should turn until it sees the wall, then look for the
-			 * "rising edges:" the points where it no longer sees the wall.
-			 * This is very similar to the FALLING_EDGE routine, but the robot
-			 * will face toward the wall for most of it.
-			 */
-			
-			//
-			// FILL THIS IN
-			//
-				
-			/* Exactly opposite of falling edge - The robot if present in a wallAbsence area, then it rotates till it sees the wall, in 
-			 * which case it starts the rising edge algorithm. The cases are the opposite - If the robot detects noWall then it 
-			 * records the angle and rotates in the opposite way, and as soon as it hits a point where it detects another wallAbsence
-			 * it records that angle and stops. Then it calculates it's actual orientation depending on these angles, based on separate cases
-			 * as given in FALLING_EDGE and adjusts all the readings. In the end, the robot is set to turn back to zero degrees.
-			 */
-						
-			
-			robot.setRotationSpeed(ROTATION_SPEED);
-			
-			if(getFilteredData() == 50){
-				wallPresence = false;
-				wallAbsence = true;
-			}
-
-			while(wallAbsence)
-			{
-				 if(getFilteredData() < 25)
-				 {
-					  wallAbsence = false;
-					  wallPresence = true;
-				 }
-			}
-			
-			Thread.sleep(500);
-			
-			while(wallPresence){
-				
-				if(getFilteredData() > 32){
-					wallAbsence = true;
-					wallPresence = false;
-				}
-			}
-			
-			odo.getPosition(position);
-			angleA=position[2];
-			
-			robot.setRotationSpeed(-ROTATION_SPEED);
-			
-			Thread.sleep(500);
-
-			wallPresence = true;
-			wallAbsence = false;
-			
-			while(wallPresence){
-				
-				if(getFilteredData() > 32){
-					wallAbsence = true;
-					wallPresence = false;
-				}
-			}
-			
-			odo.getPosition(position);
-			angleB=position[2];
-			
-			robot.setRotationSpeed(0);
-
-			odo.getPosition(position);
-			
-			if(angleA < angleB){
-				newTheta = 45-(angleA + angleB)/2;
-				//LCD.drawString("aa" + newTheta, 0, 6);
-			}
-			
-			else if(angleA > angleB){
-					newTheta = 225-(angleA + angleB)/2;
-					//LCD.drawString("bb" + newTheta, 0, 6);
-			}
-			
-			else {
-				newTheta= 45;
-				//LCD.drawString("abc", 0, 6);
-			}
-			
-			odo.getPosition(position);
-			
-			double realPosition = position[2] + (position[2] + newTheta); 
-			LCD.drawString("dd" + realPosition, 0, 7);
-			//Button.waitForAnyPress();
-			odo.setPosition(new double [] {0.0, 0.0, realPosition}, new boolean [] {false, false, true});	
-			
-			nav.turnTo(0);
-			
-		}
 		
 		}
-		
-		catch(Exception e){
 			
-		}
-		
 	}
-	
+	// methods of determining the rotations for the wheels from Lab 2's SquareDriver
+	private static int convertDistance(double radius, double distance) {
+		return (int) ((180.0 * distance) / (Math.PI * radius));
+	}
+	// methods of determining the rotating angles for the wheels from Lab 2's SquareDriver
+	private static int convertAngle(double radius, double width, double angle) {
+		return convertDistance(radius, Math.PI * width * angle / 360.0);
+	}
+
 	private int getFilteredData() {
 		int distance;
 		
@@ -262,9 +191,7 @@ public class USLocalizer {
 		us.ping();
 		
 		// wait for the ping to complete
-		try { 
-			Thread.sleep(50); 
-		} catch (InterruptedException e) {}
+		try { Thread.sleep(50); } catch (InterruptedException e) {}
 		
 		// there will be a delay here
 		distance = us.getDistance();
